e7feee6262e9b546d41b154c23713700908d1a24
Dumon <dumon.dmd@gmail.com>
2020-05-10 19:04:08 -0300

    Preparando ambiente de desenvolvimento

---
+++ resources/views/novocliente.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cadastro de clientes</title>
+	<link rel="stylesheet" type="text/css" href="{{asset('site/style.css')}}">
+</head>
+<body>
+<script type="text/javascript" src="{{asset('site/jquery.js')}}"></script>
+<script type="text/javascript" src="{{asset('site/bootstrap.js')}}"></script>
+</body>
+</html>
